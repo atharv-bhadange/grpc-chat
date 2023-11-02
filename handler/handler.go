d2a2fe04fb387a599dff3c1ca796f7c5a585a08b
atharv-bhadange <bhadange.atharv@gmail.com>
2023-11-02 18:12:04 +0530

    create server and handler

---
+++ handler/handler.go
@@ -0,0 +1,68 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	proto "github.com/atharv-bhadange/grpc-chat/gen"
+)
+
+type Connection struct {
+	proto.UnimplementedBroadcastServer
+	stream proto.Broadcast_CreateStreamServer
+	id     string
+	active bool
+	error  chan error
+}
+
+type Pool struct {
+	proto.UnimplementedBroadcastServer
+	Connection []*Connection
+}
+
+func (p *Pool) CreateStream(pconn *proto.Connect, stream proto.Broadcast_CreateStreamServer) error {
+	conn := &Connection{
+		stream: stream,
+		id:     pconn.User.Id,
+		active: true,
+		error:  make(chan error),
+	}
+
+	p.Connection = append(p.Connection, conn)
+
+	return <-conn.error
+}
+
+func (s *Pool) BroadcastMessage(ctx context.Context, msg *proto.Message) (*proto.Close, error) {
+	wait := sync.WaitGroup{}
+	done := make(chan int)
+
+	for _, conn := range s.Connection {
+		wait.Add(1)
+
+		go func(msg *proto.Message, conn *Connection) {
+			defer wait.Done()
+
+			if conn.active {
+				err := conn.stream.Send(msg)
+				fmt.Printf("Sending message to: %v from %v\n", conn.id, msg.Id)
+
+				if err != nil {
+					fmt.Printf("Error with Stream: %v - Error: %v\n", conn.stream, err)
+					conn.active = false
+					conn.error <- err
+				}
+			}
+		}(msg, conn)
+
+	}
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	<-done
+	return &proto.Close{}, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	proto "github.com/atharv-bhadange/grpc-chat/gen"
+	"github.com/atharv-bhadange/grpc-chat/handler"
+)
+
 func main() {
-	
+	// Create a new gRPC server
+	grpcServer := grpc.NewServer()
+
+	// Create a new connection pool
+	var conn []*handler.Connection
+
+	pool := &handler.Pool{
+		Connection: conn,
+	}
+
+	// Register the pool with the gRPC server
+	proto.RegisterBroadcastServer(grpcServer, pool)
+
+	// Create a TCP listener at port 8080
+	listener, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Fatalf("Error creating the server %v", err)
+	}
+
+	fmt.Println("Server started at port :8080")
+
+	// Start serving requests at port 8080
+	if err := grpcServer.Serve(listener); err != nil {
+		log.Fatalf("Error creating the server %v", err)
+	}
 }
